d39134cd90078104f38d92e7d0940e7c53b4f0d0
Fadi Alladkani <fadi.alladkani@outlook.com>
2018-01-12 17:20:54 +0200

    added basic linear interpolation, added test cases

--- soft.js
+++ soft.js
@@ -1,8 +1,14 @@
-function Smooth(max, init){
+function Smooth(init, max){
     this.max = max;
     this.init = init;
     initializeTypes();
 }
 function initializeTypes(){
     //TODO:Add other interpolation input types
+}
+Smooth.prototype.linear = function(frac){
+    if(frac > 1){
+	return this.max;
+    }
+    return frac*(this.max - this.init) + this.init;
 }
