049d57379a982333f84f5354555dde2233ad1675
Fadi Alladkani <fadi.alladkani@outlook.com>
2018-01-14 12:20:44 +0200

    Made test visual

--- test/testJS.js
+++ test/testJS.js
@@ -1,4 +1,13 @@
-var testVar = new Smooth(500, 1000);
-for(var i = 0; i < 1; i+=0.1){
-    console.log(testVar.linear(i));
+var lin = document.querySelector("#linear");
+animateMotionLin(lin, 3000);
+
+function animateMotionLin(elem, animTime){
+  var move = new Smooth(0, 700);
+  function anim(curTime){
+    elem.style.left = move.linear(curTime/animTime) + "px";
+    if(curTime >= animTime)
+      return;
+    requestAnimationFrame(anim);
+  }
+  requestAnimationFrame(anim);
 }
