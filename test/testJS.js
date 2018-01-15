942f51498ef7d28ea7f593b1fe28f71257866d5f
Fadi Alladkani <fadi.alladkani@outlook.com>
2018-01-15 22:28:33 +0200

    improved test functions

--- test/testJS.js
+++ test/testJS.js
@@ -1,10 +1,22 @@
 var lin = document.querySelector("#linear");
-animateMotionLin(lin, 3000);
+var cub = document.querySelector("#cubic");
+animateMotion(lin, "linear", 3000);
+animateMotion(cub, "cubic", 3000);
 
-function animateMotionLin(elem, animTime){
+function animateMotion(elem, type, animTime){
   var move = new Smooth(0, 700);
   function anim(curTime){
-    elem.style.left = move.linear(curTime/animTime) + "px";
+    var factor = curTime/animTime;
+    var delta;
+    switch (type){
+      case "linear":
+        delta = move.linear(factor) + "px";
+        break;
+      case "cubic":
+        delta = move.cubic(factor) +"px";
+        break;
+    }
+    elem.style.left = delta
     if(curTime >= animTime)
       return;
     requestAnimationFrame(anim);
