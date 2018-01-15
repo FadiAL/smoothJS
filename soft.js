067aeb1298724756272cc7b78621c91c941ae4cd
Fadi Alladkani <fadi.alladkani@outlook.com>
2018-01-15 22:36:53 +0200

    Added quintic smoothing

--- soft.js
+++ soft.js
@@ -16,3 +16,9 @@ Smooth.prototype.cubic = function(frac){
     return this.max;
   return (Math.pow(frac, 3)*-2 + Math.pow(frac, 2)*3)*(this.max-this.init) + this.init;
 }
+Smooth.prototype.quintic = function(frac){
+  if(frac > 1)
+    return this.max;
+  return (Math.pow(frac, 5)*6 - Math.pow(frac, 4)*15 + Math.pow(frac, 3)*10)*
+          (this.max-this.init)+this.init;
+}
