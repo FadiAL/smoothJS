547e414ce04d97964c0ec508daf50850a061fdec
Fadi Alladkani <fadi.alladkani@outlook.com>
2018-01-17 21:04:45 +0200

    added test cases

--- test/testJS.js
+++ test/testJS.js
@@ -1,34 +1,26 @@
 var DURATION = 3000;
 var DISTANCE = 1000;
+
 var lin = document.querySelector("#linear");
 var cub = document.querySelector("#cubic");
 var qui = document.querySelector("#quintic");
 var sep = document.querySelector("#septic");
-animateMotion(lin, "linear", DURATION);
-animateMotion(cub, "cubic", DURATION);
-animateMotion(qui, "quintic", DURATION);
-animateMotion(sep, "septic", DURATION);
+
+animateMotion(lin, Smooth.LINEAR, 0);//For some reason, whatever animateMotion was called
+//first will resolve Smooth.* as UNDEFINED, so call one randomly at the beginning, just
+//so that all the rest will work, don't ask me why this happens
+
+animateMotion(lin, Smooth.LINEAR, DURATION);
+animateMotion(cub, Smooth.CUBIC, DURATION);
+animateMotion(qui, Smooth.QUINTIC, DURATION);
+animateMotion(sep, Smooth.SEPTIC, DURATION);
 
 function animateMotion(elem, type, animTime){
   var move = new Smooth(0, DISTANCE);
   function anim(curTime){
     var factor = curTime/animTime;
-    var delta;
-    switch (type){
-      case "linear":
-        delta = move.linear(factor) + "px";
-        break;
-      case "cubic":
-        delta = move.cubic(factor) +"px";
-        break;
-      case "quintic":
-        delta = move.quintic(factor) + "px";
-        break;
-      case "septic":
-        delta = move.septic(factor) + "px";
-        break;
-    }
-    elem.style.left = delta
+    var delta = move.interpolate(factor, type);
+    elem.style.left = delta + "px";
     if(curTime >= animTime)
       return;
     requestAnimationFrame(anim);
